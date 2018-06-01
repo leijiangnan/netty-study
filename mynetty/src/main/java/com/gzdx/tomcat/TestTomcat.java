9c9ff4ed4b6f8fd349dd1b04780969146fbbb404
leijiang <leijiang@jd.com>
2018-06-01 17:28:04 +0800

    .

---
+++ mynetty/src/main/java/com/gzdx/tomcat/TestTomcat.java
@@ -0,0 +1,18 @@
+package com.gzdx.tomcat;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+
+public class TestTomcat {
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+
+        Connector connector = new Connector("HTTP/1.1");
+        connector.setPort(8080);
+        tomcat.setConnector(connector);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
